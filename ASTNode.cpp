51a0c7334b6a149a124c60da82ba592a17ec0f44
Ghavvvo <2gaherre12@gmail.com>
2025-04-19 20:05:48 -0400

    adding parent classes

--- ASTNode.cpp
+++ ASTNode.cpp
@@ -1,63 +1,110 @@
 #include <string.h>
 #include <iostream>
 
-class NumberNode {
+class ASTNode {
+public:
+    virtual void print(int indent = 0) const = 0;
+    virtual ~ASTNode() = default; // Destructor virtual
+};
+
+class ExpresionNode : public ASTNode {
+public:
+    virtual int evaluate() const = 0;
+    ~ExpresionNode() override = default;
+};
+class StatementNode : public ASTNode {
+public:
+    virtual void execute() const = 0;
+    ~StatementNode() override = default;
+};
+
+class NumberNode : public ExpresionNode {
 public:
     int value;
 
     NumberNode(int val) : value(val) {}
 
-    int getValue() const {
+    int evaluate() const override {
         return value;
     }
 
-    void print() const {
-        std::cout << "Number: " << getValue() << std::endl;
+    void print(int indent = 0) const override {
+        for (int i = 0; i < indent; ++i) {
+            std::cout << "  ";
+        }
+        std::cout << "Number: " << evaluate() << std::endl;
     }
 };
 
-class BinaryOperatorNode {
+class BinaryOperatorNode : public ExpresionNode {
 public:
     char op;
-    NumberNode* left;
-    NumberNode* right;
+    ExpresionNode* left;
+    ExpresionNode* right;
 
-    BinaryOperatorNode(char op, NumberNode* left, NumberNode* right) : op(op), left(left), right(right) {}
+    BinaryOperatorNode(char op, ExpresionNode* left, ExpresionNode* right) : op(op), left(left), right(right) {}
 
-    int evaluate() const {
+    int evaluate() const override {
         switch (op) {
-            case '+': return left->getValue() + right->getValue();
-            case '-': return left->getValue() - right->getValue();
-            case '*': return left->getValue() * right->getValue();
-            case '/': return left->getValue() / right->getValue();
+            case '+': return left->evaluate() + right->evaluate();
+            case '-': return left->evaluate() - right->evaluate();
+            case '*': return left->evaluate() * right->evaluate();
+            case '/': return left->evaluate() / right->evaluate();
             default: return 0;
         }
     }
 
-    void print(int indent = 0) const {
+    void print(int indent = 0) const override {
         for (int i = 0; i < indent; ++i) {
             std::cout << "  ";
         }
-        std::cout << "Result:" << evaluate() << std::endl;
         std::cout << "Operator: " << op << std::endl;
-        for (int i = 0; i < indent + 1; ++i) {
-            std::cout << "  ";
-        }
-        if (left) {
 
-            std::cout << "Left: ";
-            left->print();
-        } else {
-            std::cout << "Left: nullptr" << std::endl;
-        }
         for (int i = 0; i < indent + 1; ++i) {
             std::cout << "  ";
         }
-        if (right) {
-            std::cout << "Right: ";
-            right->print();
+        std::cout << "Left: ";
+        if (left) {
+            left->print(indent + 1);
         } else {
-            std::cout << "Right: nullptr" << std::endl;
+            std::cout << "nullptr" << std::endl;
         }
+
+        for (int i = 0; i < indent + 1; ++i) {
+            std::cout << "  ";
+        }
+        std::cout << "Right: ";
+        if (right) {
+            right->print(indent + 1);
+        } else {
+            std::cout << "nullptr" << std::endl;
+        }
+    }
+
+    ~BinaryOperatorNode() {
+        delete left;
+        delete right;
+    }
+};
+class PrintStatementNode : public StatementNode {
+public:
+    ExpresionNode* expression;
+
+    PrintStatementNode(ExpresionNode* expr) : expression(expr) {}
+
+    void execute() const override {
+        std::cout << "Print: " << expression->evaluate() << std::endl;
+    }
+
+    void print(int indent = 0) const override {
+        for (int i = 0; i < indent; ++i) {
+            std::cout << "  ";
+        }
+        std::cout << "PrintStatement:" << std::endl;
+        expression->print(indent + 1);
+    }
+
+    ~PrintStatementNode() {
+        delete expression;
     }
 };
